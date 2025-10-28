2bdbebf194c3b4988088fbea4245815201813a0f
Tireks <vmaleev2005@gmail.com>
2025-10-28 14:26:33 +0300

    First commit: project structure, types.h

---
+++ include/core/types.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <vector>
+#include <cstddef>
+#include <cstdint>
+
+namespace crypto {
+
+    using Byte = uint8_t;
+    using ByteArray = std::vector<Byte>;
+    using PermutationTable = std::vector<size_t>;
+
+    enum class BitIndexing { // правила индексирования битов
+        big_endian,
+        little_endian
+    };
+
+    enum class StartBit { // стартовы бит
+        Zero,
+        One
+    };
+
+    enum class CipherMode { // режимы шифрофания
+        ECB,
+        CBC,
+        PCBC,
+        CFB,
+        OFB,
+        CTR,
+        RandomDelta
+    };
+
+    enum class PaddingMode { // режимы набивки
+        Zeros,
+        ANSI_X923,
+        PKCS7,
+        ISO_10126
+    };
+
+}
